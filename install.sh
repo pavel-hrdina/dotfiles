20b0954170b312cc26b1ff4efd2060ffee49237f
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2024-01-27 18:24:57 +0100

    Script chores created
    Created stage one of the script, still not done.
    This stage will set up the system, so that nothing
    breaks later on, for example with the loggin manager.
    
    This part is a chore.

--- install.sh
+++ install.sh
@@ -1,25 +1,54 @@
 #!/usr/bin/env bash
 
-cat <<"EOF"
+# Script wide variables
+NO_FORMAT="\033[0m"
+F_BOLD="\033[1m"
+C_OLIVE="\033[38;5;3m"
+F_DIM="\033[2m"
 
------------------------------------------------------------------
-  _______    
-__|   __ \   
-     / .\ \   
-     \__/ |    
-   _______|     
-                                     _                    _ 
-   _      /\  /\ _   _  _ __   _ __ | |  __ _  _ __    __| |
- _| |_   / /_/ /| | | || '_ \ | '__|| | / _` || '_ \  / _` |
-|_   _| / __  / | |_| || |_) || |   | || (_| || | | || (_| |
-  |_|   \/ /_/   \__, || .__/ |_|   |_| \__,_||_| |_| \__,_|
-                 |___/ |_|                                  
-   \_______    
-__________/                                                  
+echo -e "
+----------------------------------------------------------
+   ${F_BOLD}${C_OLIVE}
+author          Pavel Hrdina     pavel.hrdina.ml@seznam.cz
+license         GPL-3.0
+   ${NO_FORMAT}
+----------------------------------------------------------
+"
 
- 	Pavel Hrdina 2024
-	Main installation script
------------------------------------------------------------------
+#############################################
+# YOU SHOULD ONLY NEED TO EDIT THIS SECTION #
+#############################################
 
-EOF
+######################################
+#Lines bellow should not be modified #
+######################################
 
+set -o pipefail
+if [ "$EUID" -ne 0 ];then
+    echo -e "${F_DIM}Please run as root${NO_FORMAT}\n"
+    exit
+fi
+
+echo -e "${F_DIM}Check operating system...${NO_FORMAT}\n"
+
+# try using zypper to determine OS
+if zypper -n --version; then
+    echo -e "\n${F_DIM}Running openSUSE, continue...${NO_FORMAT}\n";
+else
+    echo -e "\n${F_DIM}Wrong distribution, modify the scrip, and then continue...${NO_FORMAT}\n" && exit
+fi
+
+echo "----------------------------------------------------------"
+
+echo -e "${F_BOLD}STAGE ONE${NO_FORMAT}"
+echo -e "${F_BOLD}Make system ready for installation...${NO_FORMAT}"
+
+echo "----------------------------------------------------------"
+
+# Test if OS is running as server
+if [ "$(systemctl get-default)" == "graphical.target" ]; then
+    echo -e "\n${F_DIM}Running openSUSE as desktop, continue...${NO_FORMAT}\n"
+else
+    echo -e "\n${F_DIM}Running as server, change systemctl setting to desktop...${NO_FORMAT}\n"
+    systemctl isolate graphical.target
+fi
