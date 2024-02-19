c19267407932afa763587c049d4a145b5243b328
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2024-02-19 12:01:51 +0100

    change mouse conf

--- waybar/.config/waybar/reload.sh
+++ waybar/.config/waybar/reload.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-CONFIG_FILES="$HOME/.config/waybar/config .config/waybar/style.css"
+WAYBAR_FOLDER="$HOME/.config/waybar/"
+CONFIG_PATH="$WAYBAR_FOLDER/config"
+STYLE_PATH="$WAYBAR_FOLDER/style.css"
+# space-separated string of files to watch
+CONFIG_FILES="$CONFIG_PATH $STYLE_PATH"
 
 trap "killall waybar" EXIT
 
 while true; do
-    waybar &
+    waybar --config $CONFIG_PATH --style $STYLE_PATH >$WAYBAR_FOLDER/.waybar.err 2>&1 &
     inotifywait -e create,modify $CONFIG_FILES
-    killall waybar
+    killall waybar 2>/dev/null
 done
