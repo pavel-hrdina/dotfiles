cfae1d2e3fbf42266472adf634d65c424111b8b5
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2024-03-27 10:39:28 +0100

    Add more cutom configuratio
    Add custom config for bash

--- nvim/.config/nvim/lua/plugins/core.lua
+++ nvim/.config/nvim/lua/plugins/core.lua
@@ -1,3 +1,5 @@
+-- My custom configuration
+
 return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
@@ -7,6 +9,34 @@ return {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "gruvbox",
+    },
+  },
+  -- change trouble config
+  {
+    "folke/trouble.nvim",
+    -- opts will be merged with the parent spec
+    opts = {
+      multiline = true, -- render multi-line messages
+    },
+  },
+  -- add bashls to lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        bashls = {},
+      },
+    },
+  },
+  -- add any tools you want to have installed below
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "shellcheck",
+      },
     },
   },
 }
