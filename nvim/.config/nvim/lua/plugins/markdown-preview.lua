a002ed88913de62d7db0b999243f1dc8902328c8
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2024-03-28 11:18:12 +0100

    add support for markdown and markdown preview
    New file that takes care of markdown preview plugin so that it does not
    make the core.lue file a mess.

---
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,11 @@
+return {
+  -- add markdown-preview.nvim
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
+}
