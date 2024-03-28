60b8a12d7b3701a4bb553d318c8ed4f32093b6c8
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2024-03-28 11:06:04 +0100

    Add yaml support to lazivim config

--- nvim/.config/nvim/lua/plugins/core.lua
+++ nvim/.config/nvim/lua/plugins/core.lua
@@ -20,7 +20,7 @@ return {
       multiline = true, -- render multi-line messages
     },
   },
-  -- add bashls to lspconfig
+  -- add langeuage servers to lspconfig
   {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
@@ -28,32 +28,99 @@ return {
       ---@type lspconfig.options
       servers = {
         bashls = {},
+        cssls = {},
+        terraformls = {},
+        yamlls = {
+          capabilities = {
+            textDocuments = {
+              foldingRange = {
+                dynamicRegistration = false,
+                lineFoldingOnly = true,
+              },
+            },
+          },
+          -- lazy-load schemastore when needed
+          on_new_config = function(new_config)
+            new_config.settings.yaml.schemas = vim.tbl_deep_extend(
+              "force",
+              new_config.settings.yaml.schemas or {},
+              require("schemastore").yaml.schemas()
+            )
+          end,
+          redhat = { telemetry = { enabled = false } },
+          yaml = {
+            keyOrdering = false,
+            format = {
+              enable = true,
+            },
+            validate = true,
+            schemaStore = {
+              -- Must disable built-in schemaStore support to use
+              -- schemas from SchemaStore.nvim plugin
+              enable = false,
+              -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+              url = "",
+            },
+          },
+        },
+      },
+      setup = {
+        yamlls = function()
+          -- Neovim < 0.10 does not have dynamic registration for formatting
+          if vim.fn.has("nvim-0.10") == 0 then
+            LazyVim.lsp.on_attach(function(client, _)
+              if client.name == "yamlls" then
+                client.server_capabilities.documentFormattingProvider = true
+              end
+            end)
+          end
+        end,
       },
     },
-  },
-  -- add any tools you want to have installed below
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "shellcheck",
+    -- add any tools you want to have installed below
+    {
+      "williamboman/mason.nvim",
+      opts = {
+        ensure_installed = {
+          "shellcheck",
+        },
       },
     },
-  },
-  {
-    "nvimdev/dashboard-nvim",
-    event = "VimEnter",
-    opts = function(_, opts)
-      local logo = [[        
+    -- add more treesitter parsers
+    {
+      "nvim-treesitter/nvim-treesitter",
+      opts = {
+        ensure_installed = {
+          "bash",
+          "html",
+          "json",
+          "lua",
+          "markdown",
+          "markdown_inline",
+          "regex",
+          "tsx",
+          "typescript",
+          "vim",
+          "yaml",
+          "css",
+        },
+      },
+    },
+    {
+      "nvimdev/dashboard-nvim",
+      event = "VimEnter",
+      opts = function(_, opts)
+        local logo = [[        
 ░▒▓██████████►╬◄██████████▓▒░
 ░▒▓██►╔╦╦╦═╦╗╔═╦═╦══╦═╗◄██▓▒░
 ░▒▓██►║║║║╩╣╚╣═╣║║║║║╩╣◄██▓▒░
 ░▒▓██►╚══╩═╩═╩═╩═╩╩╩╩═╝◄██▓▒░
 ░▒▓██████████►╬◄██████████▓▒░
       ]]
-      logo = string.rep("\n", 8) .. logo .. "\n\n"
-      opts.config.header = vim.split(logo, "\n")
-      opts.theme = "doom"
-    end,
+        logo = string.rep("\n", 8) .. logo .. "\n\n"
+        opts.config.header = vim.split(logo, "\n")
+        opts.theme = "doom"
+      end,
+    },
   },
 }
