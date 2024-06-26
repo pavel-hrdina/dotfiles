5c7dbc94419f5d98cd2d92dc67496497b8cc094d
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2024-06-26 15:06:00 +0200

    docs: nvim config

--- nvim/.config/nvim/lua/plugins/core.lua
+++ nvim/.config/nvim/lua/plugins/core.lua
@@ -45,6 +45,7 @@ return {
       opts.root_dir = opts.root_dir
         or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
       opts.sources = vim.list_extend(opts.sources or {}, {
+        -- Add formateers here using https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md
         nls.builtins.formatting.stylua,
         nls.builtins.formatting.ocamlformat,
       })
