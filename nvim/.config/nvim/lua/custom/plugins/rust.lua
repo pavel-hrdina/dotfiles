c46bd3c167f7bab400ba63cc3c6c2635a71b2f29
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2024-07-21 18:19:40 +0200

    add lazygit to nvim and dashboard

--- nvim/.config/nvim/lua/custom/plugins/rust.lua
+++ nvim/.config/nvim/lua/custom/plugins/rust.lua
@@ -9,5 +9,20 @@ return {
     version = '^4', -- Recommended
     lazy = false, -- This plugin is already lazy
     ft = { 'rust' },
+    cmd = {},
+    server = {
+      -- Make it possible to change settings per project
+      ---@param project_root string Path to the project root
+      settings = function(project_root)
+        local ra = require 'rustaceanvim.config.server'
+        return ra.load_rust_analyzer_settings(project_root, {
+          settings_file_pattern = 'rust-analyzer.json',
+        })
+      end,
+      default_settings = {
+        -- rust-analyzer language server configuration
+        ['rust-analyzer'] = {},
+      },
+    },
   },
 }
